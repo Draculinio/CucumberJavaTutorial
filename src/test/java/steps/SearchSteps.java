aa91430cac2234782e46bd7cb03367f68211d366
Pablo Soifer <pablo.soifer@wwecorp.com>
2020-05-01 12:16:26 -0300

    First Commit

---
+++ src/test/java/steps/SearchSteps.java
@@ -0,0 +1,38 @@
+package steps;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class SearchSteps {
+	WebDriver driver;
+	@Given("the user is in the index page")
+	public void theUserIsInIndexPage() {
+		System.setProperty("webdriver.FireFox.driver", "Drivers/geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.navigate().to("http://automationpractice.com/index.php");
+	}
+	
+	@When("the user enters dresses in the search bar")
+	public void userEntersDressesInSearchBar() {
+		driver.findElement(By.id("search_query_top")).sendKeys("dresses");
+	}
+	
+	@When("the user clicks the search button")
+	public void userClicksSearchButton() {
+		driver.findElement(By.name("submit_search")).click();
+	}
+	
+	@Then("the dresses page appears")
+	public void dressesPage() {
+		String title = driver.findElement(By.className("lighter")).getText();
+		Assert.assertEquals("\"DRESSES\"", title);
+		driver.close();
+		driver.quit();
+	}
+}
